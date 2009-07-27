ce4fcb6439d4b56f6fa4181038fcb9bdf088dcee
shliutai <shliutai@126.com>
2009-07-27 06:00:10 +0000

    %1

---
+++ lab/guitest1.py
@@ -0,0 +1,19 @@
+#!/bin/python2.6
+from Tkinter import *
+def loaddata():
+    import os
+    lines = os.popen('./exp1').readlines()
+    text=''
+    for line in lines:
+        text+=line
+    return text
+def test():
+    root=Tk()
+    root.title('Test')
+    l=loaddata()
+    t=ListBox(root,text=loaddata())
+    t.pack(expand=YES,fill=BOTH)
+    root.mainloop()
+
+if __name__=='__main__':
+    test()
