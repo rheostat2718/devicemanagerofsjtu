2b596e23625f3267f75bdf6cfa21b991d8f8e61c
shliutai@126.com <shliutai@126.com@a7875d68-6629-11de-bb41-d1906ad598ae>
2009-08-08 10:33:15 +0000

    comment on module & device C API

--- trunk/lab/c_api/mod.c
+++ trunk/lab/c_api/mod.c
@@ -1,9 +1,9 @@
-#define	MODGETMAJBIND		8
 #define	MODSIZEOF_DEVID		10
 #define	MODGETDEVID		11
+devid2paths xx
 #define	MODSIZEOF_MINORNAME	12
 #define	MODGETMINORNAME		13
-#define	MODGETFBNAME		16
+
 #define	MODGETDEVFSPATH_LEN	22
 #define	MODGETDEVFSPATH		23
 #define	MODGETDEVPOLICY		27
@@ -12,5 +12,9 @@
 #define	MODGETDEVFSPATH_MI	39
 
 #define	MODRETIRE		40
+di_retire_device()
+
 #define	MODUNRETIRE		41
+di_unretire_Device()
+di_retire_T
 #define	MODISRETIRED		42
