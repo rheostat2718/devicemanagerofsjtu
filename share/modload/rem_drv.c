221924564ced5d46c625249b8a791eb0a9d852b4
shliutai <shliutai@126.com>
2009-08-27 11:42:12 +0000

    small cleanup, opensolaris in Vmware runs quite slow, remove unnecessary items...

--- share/modload/rem_drv.c
+++
@@ -1,9 +0,0 @@
-char *driver_name = argv[optind];
-int modid = getmodid();
-
-/* module is installed */
-if (modid != -1) modctl(MODUNLOAD, modid) //MODUNLOAD
-/* unload driver.conf file */
-modctl(MODUNLOADDRVCONF, (major_t)found);
-//-c
-modctl(MODREMDRVCLEANUP, driver_name, 0, NULL);
