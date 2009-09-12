076f1f274bf98dc86e2191a17b69e0f0b6bec197
shliutai@126.com <shliutai@126.com@a7875d68-6629-11de-bb41-d1906ad598ae>
2009-09-12 14:31:23 +0000

    devicec.c: get_device_info... only subtree, no minor node/ properties how can i identify each device?

--- trunk/lab/c_api/deviceuse.py
+++ trunk/lab/c_api/deviceuse.py
@@ -1,3 +1,6 @@
-import device
-list = device.getDeviceList()
-print list
+import devicec
+list = devicec.get_device_info()
+for item in list:
+    print item
+#    for (key,value) in item.items():
+#        print key,'->',value
