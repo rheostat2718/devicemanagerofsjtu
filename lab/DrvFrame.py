7a6268aee8be5cd1e37202b6ac2298746b97aff0
shliutai <shliutai@126.com>
2009-10-08 16:23:34 +0000

    update document use KeyAndValue in DrvFrame.py

--- lab/DrvFrame.py
+++ lab/DrvFrame.py
@@ -1,19 +1,22 @@
 import pygtk
-pygtk.require('2.0')
+pygtk.require( '2.0' )
 import gtk
 import drv
 import sys
 
-class DriverInfoFrame(gtk.Frame):
-    def __init__(self,devicename):
-        gtk.Frame.__init__(self)
-        self.set_label('Module Info')
-        self.set_label_align(1.0,0.0)#left top
-        self.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+class DriverInfoFrame( gtk.Frame ):
+    def __init__( self, devicename ):
+        gtk.Frame.__init__( self )
+        self.set_label( 'Module Info' )
+        self.set_label_align( 1.0, 0.0 )#left top
+        self.set_shadow_type( gtk.SHADOW_ETCHED_IN )
         self.devicename = devicename
         self.make_widgets()
 
-    def make_list(self,d):
+    def make_list( self, d ):
+        from class_DeviceManagerGUI import KeyAndValue
+        self.scrolled_window = KeyAndValue( d )
+        '''
         self.liststore = gtk.ListStore(str,str)
         for key in d.keys():
             self.liststore.append([key,d[key]])
@@ -32,49 +35,52 @@ class DriverInfoFrame(gtk.Frame):
         treecolumnkey.set_sort_column_id(0)
         treecolumnvalue.set_sort_column_id(0)
         tview.set_reorderable(True)
-        self.scrolled_window.add(tview)
         tview.show()
+        '''
 
-    def make_widgets(self):
-        btnname = ['Install','Uninstall','Refresh']
+
+    def make_widgets( self ):
+        btnname = ['Refresh', 'Install', 'Uninstall', 'Update', 'Backup', 'Restore']
         self.buttons = []
-        self.table = gtk.Table(2,len(btnname),False)
-        self.add(self.table)
+        self.table = gtk.Table( 2, len( btnname ), False )
+        self.add( self.table )
         count = 0
         for name in btnname:
-            btn = gtk.Button(name)
-            self.table.attach(btn,count,count+1,1,2)
+            btn = gtk.Button( name )
+            self.table.attach( btn, count, count + 1, 1, 2 )
             count += 1
-            self.buttons.append(btn)
+            self.buttons.append( btn )
             btn.show()
+        ''' 
         self.scrolled_window = gtk.ScrolledWindow()
         self.scrolled_window.set_border_width(10)
         self.scrolled_window.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
-        d = drv.Driver(self.devicename)
-        l = d.getInfo(True)
+        '''
+        d = drv.Driver( self.devicename )
+        l = d.getInfo( True )
         print l
-        self.make_list(l)
-        self.table.attach(self.scrolled_window,0,len(btnname),0,1)
+        self.make_list( l )
+        self.table.attach( self.scrolled_window, 0, len( btnname ), 0, 1 )
         self.scrolled_window.show()
         self.table.show()
 
-    def main(self):
+    def main( self ):
         gtk.main()
 
 class DriverGUITest():
-    def __init__(self,devicename):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.set_title('Driver GUI Test')
-        window.connect("destroy",gtk.main_quit)
-        window.set_size_request(300,300)
-        window.set_border_width(10)
-        frame = DriverInfoFrame(devicename)
-        window.add(frame)
+    def __init__( self, devicename ):
+        window = gtk.Window( gtk.WINDOW_TOPLEVEL )
+        window.set_title( 'Driver GUI Test' )
+        window.connect( "destroy", gtk.main_quit )
+        window.set_size_request( 300, 300 )
+        window.set_border_width( 10 )
+        frame = DriverInfoFrame( devicename )
+        window.add( frame )
         frame.show()
         window.show()
 
-if __name__=='__main__':
-    DriverGUITest(sys.argv[1])
+if __name__ == '__main__':
+    DriverGUITest( sys.argv[1] )
     gtk.main()
 
 """
