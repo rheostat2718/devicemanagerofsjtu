57108342b715313cbdcece2f72e32f6197310b75
shliutai@126.com <shliutai@126.com@a7875d68-6629-11de-bb41-d1906ad598ae>
2009-10-08 12:22:55 +0000

    get_parent & get_child come to work now... may build a tree in Python upon it...

--- trunk/lab/c_api/build.py
+++ trunk/lab/c_api/build.py
@@ -1,2 +1,4 @@
 from distutils.core import setup, Extension
 setup(name = 'di',version='1.0',ext_modules=[Extension('di',['di.c'])])
+setup(name = 'modulec',version='1.0',ext_modules=[Extension('modulec',['modulec.c'])])
+
