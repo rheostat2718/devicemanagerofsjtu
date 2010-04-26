c64cfa381c877dfeecc96bac9cfe07a7e35f79f3
hanzhichao2000 <hanzhichao2000@a7875d68-6629-11de-bb41-d1906ad598ae>
2010-04-26 01:47:26 +0000

    new version project/new

---
+++ project/new/Daemon.py
@@ -0,0 +1,65 @@
+import dbus
+import dbus.glib
+from dbus.mainloop.glib import DBusGMainLoop
+import gobject
+import sys
+import gtk
+import pynotify
+from Device import Device
+
+class Daemon(object):
+    def __init__(self, manager=None):
+        if manager!=None:
+            self.manager=manager
+        else:
+            self.manager=None
+
+        DBusGMainLoop(set_as_default=True)
+
+        self.bus=dbus.SystemBus()
+        obj=self.bus.get_object('org.freedesktop.Hal','/org/freedesktop/Hal/Manager')
+        self.hal_manager=dbus.Interface(obj, 'org.freedesktop.Hal.Manager')
+
+        self.hal_manager.connect_to_signal('DeviceAdded',   lambda *args:self.handle('DeviceAdded', *args))
+        self.hal_manager.connect_to_signal('DeviceRemoved', lambda *args:self.handle('DeviceRemoved', *args))
+        self.hal_manager.connect_to_signal('NewCapability', lambda *args:self.handle('NewCapability', *args))
+
+        device_names=self.hal_manager.GetAllDevices()
+
+        self.icon=gtk.StatusIcon()
+        self.icon.set_from_stock(gtk.STOCK_ABOUT)
+
+        for name in device_names:
+            self.add_dev_sig_recv(name)
+            device_dbus_obj=self.bus.get_object('org.freedesktop.Hal', name)
+            properties=device_dbus_obj.GetAllProperties(dbus_interface="org.freedesktop.Hal.Device")
+            if manager!=None
+                self.manager.append_device(Device(name, properties))
+
+    def add_dev_sig_recv(self, udi):
+        self.bus.add_signal_receiver(lambda *args: self.property_modified(udi, *args), "PropertyModified", "org.freedesktop.Hal.Device", "org.freedesktop.Hal", udi)
+
+    def property_modified(self, udi, num_changed, change_list):
+        #TODO
+        print 'in property_modified'
+        print udi, num_changed,change_list
+        pass
+
+    def loop(self):
+        self.loop=gobject.MainLoop()
+        self.loop.run()
+
+    def notify(self, title, info):
+        pynotify.init('dev')
+        n=pynotify.Notification(title, info)
+        n.attach_to_status_icon(self.icon)
+        n.show()
+
+    def handle(self, signal, udi, *args):
+        #TODO
+        self.notify(str(signal), str(udi))
+        pass
+
+if __name__=='__main__':
+    d=Daemon(None)
+    d.loop()
---
+++ project/new/Device.py
@@ -0,0 +1,68 @@
+'''File: Device.py
+data structure of device
+'''
+
+class Device(object):
+    '''class for device'''
+    SERVICE='org.freedesktop.Hal'
+    INTERFACE='org.freedesktop.Hal.Device'
+
+    def __init__(self, udi, property_=None):
+        self.udi=udi
+        self.children=[]
+        self.key_info={}
+
+        if property_==None:
+            self.property_={}
+        else:
+            self.property_=property_
+
+        try:
+            for k,v in self.property_.items():
+                if 'dbus.Array' in str(type(v)):
+                    if v!=[]:
+                        self.property_[k]=v[0]
+                    else:
+                        v=None
+        except:
+            pass
+
+        if property_.has_key("info.parent"):
+            self.key_info['parent']=str(property_["info.parent"])
+
+        if property_.has_key("info.product"):
+            self.key_info["product"]=str(property_["info.product"])
+
+        if property_.has_key("info.solaris.driver"):
+            self.key_info["type"]=str(property_["info.solaris.driver"])
+
+        if property_.has_key("info.udi"):
+            self.udi=property_["info.udi"]
+
+        if property_.has_key("info.vendor"):
+            self.key_info['vendor']=property_["info.vendor"]
+        else:
+            self.key_info["vendor"]="unknown"
+
+    def get(self, key):
+        '''get key info'''
+        if self.key_info.has_key(key):
+            return key_info[key]
+        else:
+            return "None"
+
+    def has_children(self):
+        '''check if has chilren'''
+        return self.children!=[]
+
+    def append_child(self, child):
+        '''append a child'''
+        self.children.append(child)
+
+    def print_tree(self, space):
+        '''print structure of device tree'''
+        print space
+        print "[%s]"%(self.get("product"))
+
+        for child in self.children:
+            child.print_tree("    "+space)
