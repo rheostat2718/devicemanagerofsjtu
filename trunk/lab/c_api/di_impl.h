f69816a036a149c896cdc82371287a33a3b154d7
stlt1sean <stlt1sean@a7875d68-6629-11de-bb41-d1906ad598ae>
2010-05-02 17:16:39 +0000

    add prop/minor support in di.c

--- trunk/lab/c_api/di_impl.h
+++ trunk/lab/c_api/di_impl.h
@@ -5,6 +5,7 @@
 #include <structmember.h>
 #include <libdevinfo.h>
 #include <stdio.h>
+#include <strings.h>
 
 #ifndef PyMODINIT_FUNC
 #define PyMODINIT_FUNC void
@@ -27,6 +28,7 @@ typedef struct {
 	di_minor_t minor;
 } minorobj;
 
+/*
 typedef struct {
 	PyObject_HEAD
 	di_path_t path;
@@ -46,7 +48,7 @@ typedef struct {
 	PyObject_HEAD
 	di_prom_prop_t promprop;
 } prompropobj;
-
+*/
 typedef struct {
 	PyObject_HEAD
 	di_prop_t prop;
@@ -55,11 +57,13 @@ typedef struct {
 /* pointer types */
 #define nodeobj_t nodeobj*
 #define minorobj_t minorobj*
+/*
 #define pathobj_t pathobj*
 #define lnodeobj_t lnodeobj*
 #define linkobj_t linkobj*
-#define propobj_t propobj*
 #define prompropobj_t prompropobj*
+*/
+#define propobj_t propobj*
 
 /* Node type operations */
 static PyObject* node_new(PyTypeObject *type,PyObject *args,PyObject *kwds);
@@ -85,30 +89,35 @@ static PyObject * node_nodeid(PyObject * self,PyObject *args);
 static PyObject * node_driver_major(PyObject * self,PyObject *args);
 static PyObject * node_state(PyObject * self,PyObject *args);
 static PyObject * node_distate(PyObject * self,PyObject *args);
-static PyObject * node_devid(PyObject * self,PyObject *args);
+//static PyObject * node_devid(PyObject * self,PyObject *args);
 static PyObject * node_devfs_path(PyObject * self,PyObject *args);
 static PyObject * node_driver_name(PyObject * self,PyObject *args);
 static PyObject * node_driver_ops(PyObject * self,PyObject *args);
 static PyObject * node_proplist(PyObject * self,PyObject *args);
+static PyObject * node_minorlist(PyObject * self,PyObject *args);
 
 static PyMethodDef node_methods[]={
 		{"get_info",(PyCFunction)node_info,0,"Return node info"},
-		{"get_child",(PyCFunction)node_child,1,"Return list of child nodes"},
+		{"get_children",(PyCFunction)node_child,1,"Return a list of children nodes"},
 		{"get_parent",(PyCFunction)node_parent,1,"Return parent node"},
-		{"get_prop",(PyCFunction)node_proplist,1,"Return properties"},
-		{"node_name",(PyCFunction)node_name,0,"Return node name"},
-		{"bus_addr",(PyCFunction)node_bus_addr,0,"Return bus address"},
-		{"binding_name",(PyCFunction)node_binding_name,0,"Return binding name"},
-		{"compatible_names",(PyCFunction)node_compatible_names,0,"Return a list of compatible names"},
-		{"instance",(PyCFunction)node_instance,0,"Return number of instance"},
-		{"nodeid",(PyCFunction)node_nodeid,0,"Return nodeid"},
-		{"driver_major",(PyCFunction)node_driver_major,0,"Return driver major number"},
-		{"node_state",(PyCFunction)node_state,0,"Return node state"},
+		{"get_prop",(PyCFunction)node_proplist,1,"Return node properties"},
+		{"get_minor",(PyCFunction)node_minorlist,1,"Return minor nodes"},
+		{"get_bus_addr",(PyCFunction)node_bus_addr,0,"Return bus address (di_bus_addr)"},
+		{"get_binding_name",(PyCFunction)node_binding_name,0,"Return binding name (di_binding_name)"},
+		{"get_compatible_name_list",(PyCFunction)node_compatible_names,0,"Return a list of compatible names (di_compatible_names)"},
+		// (obsolete) di_devid, ignored
+		{"get_driver_name",(PyCFunction)node_driver_name,0,"Return driver name, None for no driver (di_driver_name)"},
+		{"get_driver_ops",(PyCFunction)node_driver_ops,0,"Return driver options (di_driver_ops)"},
+		{"get_driver_major",(PyCFunction)node_driver_major,0,"Return driver major number, None for no driver (di_driver_major)"},
+		{"get_instance_num",(PyCFunction)node_instance,0,"Return instance number, -1 for not assigned (di_instance)"},
+		{"get_nodeid",(PyCFunction)node_nodeid,0,"Return type of device (di_nodeid)"},
+		{"get_node_name",(PyCFunction)node_name,0,"Return node name (di_node_name)"},
+
+		// value in header file, but not in manual
+		{"node_state",(PyCFunction)node_state,0,"Return node state <not in Solaris manual>"},
 		{"state",(PyCFunction)node_distate,0,"Return state <not in Solaris manual>"},
-		{"devid",(PyCFunction)node_devid,0,"Return node devid"},
+
 		{"devfs_path",(PyCFunction)node_devfs_path,0,"Return device file path under /devices"},
-		{"driver_name",(PyCFunction)node_driver_name,0,"Return driver name"},
-		{"driver_ops",(PyCFunction)node_driver_ops,0,"Return driver options"},
 		{NULL} /*Sentinel*/
 };
 
@@ -176,10 +185,14 @@ static PyMemberDef prop_members[]={
 /* Prop type methods */
 static PyObject * prop_name(PyObject *self,PyObject *args);
 static PyObject * prop_type(PyObject *self,PyObject *args);
+static PyObject * prop_type_str(PyObject *self,PyObject *args);
+static PyObject * prop_value(PyObject *self,PyObject *args);
 
 static PyMethodDef prop_methods[]={
-		{"get_name",(PyCFunction)prop_name,0,"Return property name"},
-		{"get_type",(PyCFunction)prop_type,0,"Return property type id"},
+		{"get_name",(PyCFunction)prop_name,0,"Return property name (di_prop_name)"},
+		{"get_type",(PyCFunction)prop_type,0,"Return property type id (di_prop_type)"},
+		{"get_type_name",(PyCFunction)prop_type_str,0,"Return property type name"},
+		{"get_value",(PyCFunction)prop_value,0,"Return property value(di_prop_bytes, ints, int64, strings)"},
 		{NULL} /*Sentinel*/
 };
 
@@ -231,6 +244,83 @@ static PyTypeObject PropType = {/* type header */
 		(initproc) prop_init,/*tp_init*/
 		0,/*tp_alloc*/
 		prop_new,/*tp_new*/
+};
+
+/* Minor type operations */
+static PyObject* minor_new(PyTypeObject *type,PyObject *args,PyObject *kwds);
+static int minor_clear(minorobj_t self);
+static void minor_dealloc(minorobj_t self);
+static int minor_init(minorobj_t self,PyObject *args,PyObject *kwds);
+
+static PyMemberDef minor_members[]={
+		{"minor",T_OBJECT_EX,offsetof(minorobj,minor),0,"minor"},
+		{NULL}/* Sentinel */
+};
+
+/* Minor type methods */
+static PyObject * minor_devfs_path(PyObject * self,PyObject *args);
+static PyObject * minor_name(PyObject * self,PyObject *args);
+static PyObject * minor_nodetype(PyObject * self,PyObject *args);
+static PyObject * minor_spectype(PyObject * self,PyObject *args);
+static PyObject * minor_info(PyObject * self,PyObject *args);
+
+static PyMethodDef minor_methods[]={
+		{"get_name",(PyCFunction)minor_name,0,"Return minor node name (di_minor_name)"},
+		{"get_nodetype",(PyCFunction)minor_nodetype,0,"Return minor node nodetype (di_minor_nodetype)"},
+		{"get_spectype",(PyCFunction)minor_spectype,0,"Return minor node spectype (di_minor_spectype)"},
+		{"get_devfs_path",(PyCFunction)minor_devfs_path,0,"Return minor node devfs path (di_devfs_minor_path)"},
+		{"get_info",(PyCFunction)minor_info,0,"Return minor node info"},
+		{NULL} /*Sentinel*/
+};
+
+/* Minor type descriptors */
+static PyTypeObject MinorType = {/* type header */
+		PyObject_HEAD_INIT(NULL) /*PyObject_HEAD_INIT(&PyType_Type)*/
+		0,/* ob_size */
+		"di.Minor",/* tp_name */
+		sizeof(minorobj),/* tp_basicsize */
+		0,/* tp_itemsize */
+
+		/* standard methods */
+		(destructor)minor_dealloc,/* tp_dealloc */
+		0,/* tp_print */
+		0,/* tp_getattr */
+		0,/* tp_setattr */
+		0,/*tp_compare*/
+		0,/*tp_repr*/
+
+		/* type categories*/
+		0,/*tp_as_number*/
+		0,/*tp_as_sequence*/
+		0,/*tp_as_mapping*/
+
+		/* more methods */
+		0,/*tp_hash*/
+		0,/*tp_call*/
+		0,/*tp_str*/
+		0,/*tp_getattro*/
+		0,/*tp_setattro*/
+		0,/*tp_as_buffer*/
+
+		Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,/*tp_flags*/
+		"di_minor_t objects",/*tp_doc*/
+		0,/*tp_traverse*/
+		(inquiry)minor_clear,/*tp_clear*/
+		0,/*tp_richcompare*/
+		0,/*tp_weaklistoffset*/
+		0,/*tp_iter*/
+		0,/*tp_iternext*/
+		minor_methods,/*tp_methods*/
+		minor_members,/*tp_members*/
+		0,/*tp_getset*/
+		0,/*tp_base*/
+		0,/*tp_dict*/
+		0,/*tp_descr_get*/
+		0,/*tp_descr_set*/
+		0,/*tp_dictoff*/
+		(initproc) minor_init,/*tp_init*/
+		0,/*tp_alloc*/
+		minor_new,/*tp_new*/
 };
 
 #endif
--- trunk/lab/c_api/diuse.py
+++ trunk/lab/c_api/diuse.py
@@ -5,10 +5,15 @@ print di.DI_BUS_DOWN
 print dir( di.Node )
 root = di.Node()
 print root.get_info()
-children = root.get_child()
+children = root.get_children()
 print len( children )
 for child in children:
-    print child.get_info()
+    print 'N\t', child.get_info()
     proplist = child.get_prop()
+    print "property count: ", len( proplist )
     for prop in proplist:
-        print prop.get_name(), prop.get_type()
+        print 'P\t\t', prop.get_name(), ',', prop.get_type_name(), ',', prop.get_value()
+    minorlist = child.get_minor()
+    print "minor-node count: ", len( minorlist )
+    for minor in minorlist:
+        print 'M\t\t', minor.get_info()
