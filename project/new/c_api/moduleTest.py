d3f2e54426c8f54dda2f71d5f359c525c8126624
stlt1sean <stlt1sean@a7875d68-6629-11de-bb41-d1906ad598ae>
2010-05-04 09:47:52 +0000

    move some parts of lib/drv.py into new/Driver.py, with module infomation~~

---
+++ project/new/c_api/moduleTest.py
@@ -0,0 +1,28 @@
+import modulec
+
+def test():
+    print 'test ( 1 / 4 ) getModuleInfo()'
+    print 'module 250 info', modulec.getModuleInfo( 250 )
+    try:
+        print 'module 1234 info', modulec.getModuleInfo( 1234 )
+    except SystemError:
+        print 'caught expecting SystemError'
+
+    print 'test ( 2 / 4 ) getModuleId()'
+    print 'id for ipc', modulec.getModuleId( 'ipc' )
+    print 'id for asdf', modulec.getModuleId( 'asdf' )
+
+    print 'test ( 3 / 4 ) getMajorName()'
+    print 'name of major number 4', modulec.getMajorName( 4 )
+    try:
+        print 'name of major number 1234', modulec.getMajorName( 1234 )
+    except SystemError:
+        print 'caught expecting SystemError'
+
+    print 'test ( 4 / 4 ) getModPathLen(), getModPathLen()'
+    print 'module path len', modulec.getModPathLen()
+    print 'module path', modulec.getModPath()
+
+if __name__ == '__main__':
+    test()
+
--- project/new/tools.py
+++ project/new/tools.py
@@ -1,6 +1,6 @@
 #!/bin/env python2.6
 import os
-
+import logging
 """
 This file provides a list of tools.
 """
@@ -14,7 +14,8 @@ def reconfigure():
     if os.geteuid() != 0:
         return False
     try:
-        fd = os.open( '/reconfigure', os.O_RDONLY | os.O_CREAT )
+        logging.debug( 'reconfigure' )
+        fd = os.open( '/reconfigure', os.O_WRONLY | os.O_CREAT )
         os.close( fd )
     except OSError:
         return False
@@ -31,3 +32,80 @@ def localerun( func, localestr = 'en_GB.UTF-8' ):
     os.environ['LC_MESSAGES'] = old
     return value
 
+def run_devfsadm():
+    if os.geteuid() != 0:
+        return False
+    try:
+        logging.debug( 'devfsadm' )
+        ret = os.system( "devfsadm -v" )
+    except OSError:
+        return False
+    return ( ret == 0 )
+
+def run_remdrv( drvname, basedir = None ):
+    logging.debug( 'rem_drv ' + drvname )
+    if basedir:
+        opt = " -b " + basedir + ' '
+    else:
+        opt = ' '
+    ret = os.system( 'rem_drv ' + opt + drvname )
+    return ret
+
+def run_adddrv( drvname, basedir = None, classname = None, identifyname = None, permission = None, noload = False, policy = None, privilege = None, verbose = True ):
+    logging.debug( 'add_drv ' + drvname )
+    if basedir:
+        opt = " -b " + basedir + ' '
+    else:
+        opt = ' '
+    if classname:
+        opt = opt + " -c " + classname + ' '
+    if identifyname:
+        #seperated by white space
+        opt = opt + " -i '" + identifyname + "' "
+    if permission:
+        opt = opt + " -m '" + permission + "' "
+    if noload:
+        opt = opt + " -n "
+    if policy:
+        opt = opt + " -p '" + policy + "' "
+    if privilege:
+        opt = opt + " -P '" + privilege + "' "
+    if verbose:
+        opt = opt + " -v "
+    ret = os.system( 'add_drv ' + opt + drvname )
+    if basedir:
+        reconfigure()
+    return ret
+
+def run_updatedrv( drvname, basedir = None, change = None, identifyname = None, permission = None, noload = False, policy = None, privilege = None, verbose = True ):
+    logging.debug( 'update_drv ' + drvname )
+    if basedir:
+        opt = " -b " + basedir + ' '
+    else:
+        opt = ' '
+    if noload:
+        opt = opt + " -n "
+    if verbose:
+        opt = opt + " -v "
+
+    if change:
+        if change == 'a':
+            opt = opt + ' -a '
+        elif change == 'd':
+            opt = opt + ' -d '
+        else:
+            return - 1
+        if identifyname:
+            #seperated by white space
+            opt = opt + " -i '" + identifyname + "' "
+        if permission:
+            opt = opt + " -m '" + permission + "' "
+        if policy:
+            opt = opt + " -p '" + policy + "' "
+        if privilege:
+            opt = opt + " -P '" + privilege + "' "
+
+    ret = os.system( 'update_drv ' + opt + drvname )
+    if basedir:
+        reconfigure()
+    return ret
